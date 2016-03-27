d2addb329cd3ee6ea3fbf88888ff8b59b47d4b5c
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2016-03-27 07:07:44 +0200

    Added Partial FFI support

---
+++ garrysmod/optional_ffi_support.lua
@@ -0,0 +1,35 @@
+if TrackIR_Update then return end
+require('ffi')
+ffi.cdef[[
+	int trackIR_Pitch();
+	int trackIR_Roll();
+	int trackIR_Yaw();
+	int trackIR_X();
+	int trackIR_Y();
+	int trackIR_Z();
+	int trackIR_Update();
+	int trackIR_NPStatus();
+	int trackIR_Init();
+]]
+TrackIR = TrackIR or {}
+
+TrackIR.lib = TrackIR.lib or ffi.load("TrackIR_FFI")
+
+TrackIR_Ver =  function() return "Not working with FFI" end
+TrackIR_Debug = function() return "Not working with FFI" end
+TrackIR_LostFrames = function() return -42 end
+TrackIR_Pitch = TrackIR.lib.trackIR_Pitch
+TrackIR_Yaw = TrackIR.lib.trackIR_Yaw
+TrackIR_Roll = TrackIR.lib.trackIR_Roll
+TrackIR_X = TrackIR.lib.trackIR_X
+TrackIR_Y = TrackIR.lib.trackIR_Y
+TrackIR_Z = TrackIR.lib.trackIR_Z
+TrackIR_Update = TrackIR.lib.trackIR_Update
+TrackIR_Status = TrackIR.lib.trackIR_NPStatus
+
+
+if not TrackIR.lib.trackIR_Init then
+	print("failed to initialize TrackIR FFI")
+else
+	print("INITED DIS MATE")
+end
